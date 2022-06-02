8060072f9c9c82e39a48a8378ab26936c28320ec
Kadir <kadirafraj@gmail.com>
2022-06-02 16:24:32 +0530

    American guy

--- tests/test_End2End.py
+++ tests/test_End2End.py
@@ -4,6 +4,7 @@ from utilities.BaseClass import BaseClass
 
 class TestOne(BaseClass):
     def test_e2e(self):
+        print("Hello World")
         log=self.getlogger()
         homepage= Homepage(self.driver)
         checkoutpage=homepage.shopitems()
